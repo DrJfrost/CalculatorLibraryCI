0f22cc8c186d0109ca67595c73a4b6efcfd195c2
DrJfrost <julianrojas99@hotmail.com>
2020-11-17 19:37:43 -0500

    Added division module

--- test_calculator.py
+++ test_calculator.py
@@ -8,10 +8,13 @@ import calculator
 class TestCalculator:
 
     def test_addition(self):
-        assert 4 == calculator.add(2, 2)
+        assert calculator.add(2, 2) == 4
 
     def test_subtraction(self):
-        assert 2 == calculator.subtract(4, 2)
+        assert calculator.subtract(4, 2) == 2
 
     def test_multiplication(self):
-        assert 100 == calculator.multiply(10, 10)
+        assert calculator.multiply(10, 10) == 100
+
+    def test_division(self):
+        assert calculator.divide(3,3) == 1
